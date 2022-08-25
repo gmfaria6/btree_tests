1e7b71142677de4810445fcf850d0c1557aa7d5e
Gabriel Matheus <gmfaria6@gmail.com>
2022-08-25 20:39:28 -0300

    adding the b_tree results

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# btree_tests
+<br />
+<div align="center">
+  <h3 align="center">B-Trees</h3>
+
+  <p align="center">
+    B and B+ trees implementations in Python.
+  </p>
+</div>
---
+++ btree_test.py
@@ -0,0 +1,42 @@
+from b_tree.btree import BTree
+import random
+import json
+import time
+
+for n_elements in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
+    for test in range(1, 31):
+        print("Running {} elements test {}".format(n_elements, test))
+        btree = BTree(3)
+        elements = []
+
+        tree_info = {}
+
+        random.seed(5)
+
+        for i in range(0, n_elements):
+            elements.append(random.randint(0, n_elements+1))
+
+        init_time = time.time()
+
+        avg_insert_time = 0
+
+        for element in elements:
+            insert_init_time = time.time()
+            btree.insert(element)
+            avg_insert_time += time.time() - insert_init_time
+            # print(btree.height)
+            # print(btree)
+        avg_insert_time = avg_insert_time/len(elements)
+
+        end_time_all_insert = time.time() - init_time
+        # print("btree")
+        # print(btree)
+
+        # print(btree.search(39))
+        # print(btree.search(43))
+
+        tree_info["height"] = btree.height
+        tree_info["insert_avg_time"] = avg_insert_time
+        tree_info["total_insert_time"] = end_time_all_insert
+
+        json.dump(tree_info, open("{}_elements/btree_test_{}.txt".format(len(elements), test), 'w'))
